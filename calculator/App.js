5c31c3f9de26e0cd61d8cbb841cfb1be6931a899
jnguyen3436 <jnguyen3436@gmail.com>
2020-01-22 16:10:59 -0500

    made the layout of the calculator

--- calculator/App.js
+++ calculator/App.js
@@ -1,24 +1,52 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Text, View } from 'react-native';
 
 export default class App extends Component {
+
+  constructor(){
+    super()
+    this.state={}
+  }
+
 render(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+        <View style={styles.container}>
+        <View style={styles.result}></View>
+        <View style={styles.calculation}></View>
+        <View style={styles.buttons}>
+        <View style={styles.numbers}></View>
+        <View style={styles.operations}></View>
+        </View>
+      </View>
   );
 }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1
   },
-});
+  result:{
+    flex:2,
+    backgroundColor:'red'
+  },
+  calculation:{
+    flex: 1,
+    backgroundColor:'green'
+  },
+  buttons:{
+    flexGrow:7,
+    flexDirection:'row'
+  },
+  numbers:{
+    flex:3,
+    backgroundColor:'yellow'
+  },
+  operations:{
+    flex:1,
+    backgroundColor:'black'
+  }
+})
 
 
 // import React, { Component } from 'react';
