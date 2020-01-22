04065f3e20c7d28680be5b92299d90a3d73e78a6
jnguyen3436 <jnguyen3436@gmail.com>
2020-01-22 16:26:07 -0500

    styling and added text to calculations

--- calculator/App.js
+++ calculator/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet,Text, View } from 'react-native';
+import { StyleSheet,Text, View,Button } from 'react-native';
 
 export default class App extends Component {
 
@@ -11,11 +11,42 @@ export default class App extends Component {
 render(){
   return (
         <View style={styles.container}>
-        <View style={styles.result}></View>
-        <View style={styles.calculation}></View>
+        <View style={styles.result}>
+            <Text style={styles.resultText}>11*11</Text>
+        </View>
+        <View style={styles.calculation}>
+          <Text style={styles.calculationText}>121</Text>
+        </View>
         <View style={styles.buttons}>
-        <View style={styles.numbers}></View>
-        <View style={styles.operations}></View>
+        <View style={styles.numbers}>
+          <View style={styles.row}>
+              <Button title="0"/>
+              <Button title="0"/>
+              <Button title="0"/>
+          </View>
+          <View style={styles.row}>
+              <Button title="0"/>
+              <Button title="0"/>
+              <Button title="0"/>
+          </View>
+          <View style={styles.row}>
+              <Button title="0"/>
+              <Button title="0"/>
+              <Button title="0"/>
+          </View>
+          <View style={styles.row}>
+              <Button title="0"/>
+              <Button title="0"/>
+              <Button title="0"/>
+          </View>
+        </View>
+        <View style={styles.operations}>
+        
+              <Button title="+"/>
+              <Button title="-"/>
+              <Button title="x"/>
+              <Button title="÷"/>
+        </View>
         </View>
       </View>
   );
@@ -26,13 +57,31 @@ const styles = StyleSheet.create({
   container:{
     flex:1
   },
+  row:{
+    flexDirection:'row',
+    flex:1,
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  calculationText:{
+    fontSize:14,
+    color: 'white'
+  },
+  resultText:{
+    fontSize:20,
+    color: 'white'
+  },
   result:{
     flex:2,
-    backgroundColor:'red'
+    backgroundColor:'red',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
   },
   calculation:{
     flex: 1,
-    backgroundColor:'green'
+    backgroundColor:'green',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
   },
   buttons:{
     flexGrow:7,
@@ -44,7 +93,9 @@ const styles = StyleSheet.create({
   },
   operations:{
     flex:1,
-    backgroundColor:'black'
+    backgroundColor:'black',
+    justifyContent: 'space-around',
+
   }
 })
 
